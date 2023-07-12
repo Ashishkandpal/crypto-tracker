ab79d145e7ae9e4ef830aaddd34e1ebc9f29f475
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-07-12 10:43:34 +0530

    news api is working only in localhost so news api changed

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -11,18 +11,37 @@ const News = () => {
   const randomGen = () => {
     return Math.floor(Math.random() * 50);
   };
+
+  // works only on local host
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=9fee024676304f6fa01a60eee032df4c"
+  //     )
+  //     .then((res) => {
+  //       setNewsData(res.data.articles);
+  //       setIsLoading(false);
+  //       const jsonString = JSON.stringify(res.data.articles);
+
+  //       // Create a Blob from the JSON string
+  //       const blob = new Blob([jsonString], { type: "application/json" });
+
+  //       // Trigger the download using FileSaver.js
+  //       saveAs(blob, "data.json");
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
   useEffect(() => {
     axios
-      .get(
-        "https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=9fee024676304f6fa01a60eee032df4c"
-      )
+      .get("/data.json")
       .then((res) => {
-        setNewsData(res.data.articles);
+        setNewsData(res.data);
         setIsLoading(false);
       })
-      .catch((err) => {
-        console.log(err);
-      });
+      .catch((err) => console.log(err));
   }, []);
 
   for (let i = 0; i < 10; i++) {
--- src/components/common/BackToTop/BackToTop.js
+++ src/components/common/BackToTop/BackToTop.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ArrowUpwardRoundedIcon from "@mui/icons-material/ArrowUpwardRounded";
 
 import "./BackToTop.css";
 
 function BackToTop() {
-  let mybutton = document.getElementById("myBtn");
+  // let mybutton = document.getElementById("myBtn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
-  };
+  // // When the user scrolls down 20px from the top of the document, show the button
+  // window.onscroll = function () {
+  //   scrollFunction();
+  // };
 
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      mybutton.style.display = "flex";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
+  // function scrollFunction() {
+  //   if (
+  //     document.body.scrollTop > 20 ||
+  //     document.documentElement.scrollTop > 20
+  //   ) {
+  //     mybutton.style.display = "flex";
+  //   } else {
+  //     mybutton.style.display = "none";
+  //   }
+  // }
+
+  useEffect(() => {
+    const mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    const scrollFunction = () => {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        mybutton.style.display = "flex";
+      } else {
+        mybutton.style.display = "none";
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", scrollFunction);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", scrollFunction);
+    };
+  }, []); // Empty dependency array to run the effect only once
 
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
